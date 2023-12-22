3f60f0fc69d6d3d7e27ba8049b6cfb0a5ffbb01b
haryulina <hrylnaa@gmail.com>
2023-12-22 11:18:27 +0700

    report

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -58,9 +59,7 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-
-
-
+    implementation("com.github.bumptech.glide:glide:4.12.0")
 
 
 
--- app/src/main/java/com/user/phisheye/Data/API/ApiService.kt
+++ app/src/main/java/com/user/phisheye/Data/API/ApiService.kt
@@ -1,8 +1,12 @@
 package com.user.phisheye.Data.API
 
 import com.user.phisheye.Data.Pref.Login
+import com.user.phisheye.Data.Pref.PredictPhishingResponse
 import com.user.phisheye.Data.Pref.Register
+import com.user.phisheye.Data.Pref.Report
 import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -23,4 +27,15 @@ interface ApiService {
         @Field("email") email: String,
         @Field("password") password: String
     ): Call<Register>
+
+    @POST("reportLink")
+    @FormUrlEncoded
+    fun report(
+        @Field("link") name: String
+    ): Call<Report>
+
+    @POST("/predict")
+    suspend fun predictPhishing(
+        @Body requestBody: Map<String, String>
+    ): Response<PredictPhishingResponse>
 }
